b68ae484a73f0313ef265923d0741fd393d2c831
Brandon Williams <nis350ztt@gmail.com>
2020-07-16 19:31:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,5 @@
 def reverse_each_word(sentence)
   return sentence.to_a 
-  sentence.collect.reverse do |reverse|
-    "Hello there, and how are you?"
-  end 
-  
-def test(sentence)
-  sentence.split.map {|word| word.reverse}.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
 end 
